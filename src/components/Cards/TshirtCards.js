35ef8f7b3acdee222c0b714288f1867748d49827
Chad Cook <chcook@redventures.com>
2026-02-11 14:51:52 -0500

    changed tshirt card backgrounds to notate sizing

--- src/components/Cards/TshirtCards.js
+++ src/components/Cards/TshirtCards.js
@@ -21,7 +21,7 @@ function TshirtCards({ userPoint, showVotes }) {
             aria-label={`Vote ${size}`}
             aria-pressed={userPoint === size}
         >
-            <span className="__cards__card__box">
+            <span className={`__cards__card__box __cards__card__box--${size.toLowerCase()}`}>
                 <span className="__cards__card__label">{size}</span>
             </span>
         </button>
